ea12ee1657ea197971805c7f289968990c1c4ae2
weekendchow <mozhou216@gmail.com>
2017-08-20 00:11:34 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# Neighborhood Map_Skateboarding in Ottawa 
+---
+This is a single page application project for Udacity Front-end Nonodegree using knockoutJS framework & Google Maps API to implement.
+
+## Features
+1. Google Map implimentation shows Ottawa stateboard park locations with markers
+2. List along sidebar shows the park's name and address.
+3. Toggle button which can hide and show list.
+3. Search function to filter the location.
+4. Populate info window shows the infomation of the park.
+5. Customized map style
+6. Responsive 
+
+## APIs
+1. Google Maps API to implement the map and related function.
+2. Download JSON format data and build the API with GitHub Pages.(https://weekendchow.github.io/skateboard-parks-API/skateboard-parks.json)
+ 
+## Running
+Clone or download this repo and open the index.html file. Or open it [live]().
+
+
+## Attribution
+1. [Open Data Ottawa](http://data.ottawa.ca/dataset/skateboard-parks/resource/8c26bbf7-51d9-4b54-997a-c472c5b6d355)
+2. [SNAZZY MAPS (Lost in the desert-Diana Caballero)](https://snazzymaps.com/style/93/lost-in-the-desert)
+
+## Reference
+1. [Google Maps API Doc](https://developers.google.com/maps/documentation/)
+2. [KnockoutJS Doc](http://knockoutjs.com/documentation/introduction.html)
+3. [MDN JS Guide](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ js/app.js
@@ -0,0 +1,372 @@
+//Google map
+var map;
+
+//Initializes the Google Map object and activates Knockout
+function initMap() {
+    //Set up map style
+    var styles = [{
+            "elementType": "labels",
+            "stylers": [{
+                    "visibility": "off"
+                },
+                {
+                    "color": "#f49f53"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "stylers": [{
+                    "color": "#f9ddc5"
+                },
+                {
+                    "lightness": -7
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "stylers": [{
+                    "color": "#813033"
+                },
+                {
+                    "lightness": 43
+                }
+            ]
+        },
+        {
+            "featureType": "poi.business",
+            "stylers": [{
+                    "color": "#645c20"
+                },
+                {
+                    "lightness": 38
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "stylers": [{
+                    "color": "#1994bf"
+                },
+                {
+                    "saturation": -69
+                },
+                {
+                    "gamma": 0.99
+                },
+                {
+                    "lightness": 43
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                    "color": "#f19f53"
+                },
+                {
+                    "weight": 1.3
+                },
+                {
+                    "visibility": "on"
+                },
+                {
+                    "lightness": 16
+                }
+            ]
+        },
+        {
+            "featureType": "poi.business"
+        },
+        {
+            "featureType": "poi.park",
+            "stylers": [{
+                    "color": "#645c20"
+                },
+                {
+                    "lightness": 39
+                }
+            ]
+        },
+        {
+            "featureType": "poi.school",
+            "stylers": [{
+                    "color": "#a95521"
+                },
+                {
+                    "lightness": 35
+                }
+            ]
+        },
+        {},
+        {
+            "featureType": "poi.medical",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                    "color": "#813033"
+                },
+                {
+                    "lightness": 38
+                },
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {
+            "elementType": "labels"
+        },
+        {
+            "featureType": "poi.sports_complex",
+            "stylers": [{
+                    "color": "#9e5916"
+                },
+                {
+                    "lightness": 32
+                }
+            ]
+        },
+        {},
+        {
+            "featureType": "poi.government",
+            "stylers": [{
+                    "color": "#9e5916"
+                },
+                {
+                    "lightness": 46
+                }
+            ]
+        },
+        {
+            "featureType": "transit.station",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        },
+        {
+            "featureType": "transit.line",
+            "stylers": [{
+                    "color": "#813033"
+                },
+                {
+                    "lightness": 22
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "stylers": [{
+                "lightness": 38
+            }]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                    "color": "#f19f53"
+                },
+                {
+                    "lightness": -10
+                }
+            ]
+        },
+        {},
+        {},
+        {}
+    ];
+
+    //Create map object
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 45.416667,
+            lng: -75.7
+        },
+        zoom: 13,
+        styles: styles,
+        mapTypeControl: false
+    });
+
+    //Activate Knockout
+    ko.applyBindings(new MapViewModel());
+
+}
+
+//DataObject of each SkateboardPark
+function SkateboardPark(dataObj) {
+    var self = this;
+    self.name = dataObj.properties.NAME;
+    self.name_fr = dataObj.properties.NAME_FR;
+    self.address_fr = dataObj.properties.ADDRESS_FR;
+    self.address = dataObj.properties.ADDRESS;
+    self.parkID = dataObj.properties.PARK_ID;
+    self.notes = dataObj.properties.NOTES;
+    self.latitude = parseFloat(dataObj.geometry.coordinates[1]);
+    self.longitude = parseFloat(dataObj.geometry.coordinates[0]);
+
+    //Create the map marker for SkateboardPark object
+    self.mapMarker = new google.maps.Marker({
+        position: {
+            lat: self.latitude,
+            lng: self.longitude
+        },
+        map: map,
+        title: self.name,
+        animation: google.maps.Animation.DROP,
+    });
+
+    //Create the infoWindow for SkateboardPark object
+    self.infoWindow = new google.maps.InfoWindow();
+
+    //Function that populate the infoWindow
+    self.populateInfoWindow = function() {
+        if (!self.infoWindow.getContent()) {
+            //Create the content of infoWindow
+            var content = '<h3 class="info-title">' + self.name + ' / ' + self.name_fr + '</h3>';
+            content += '<p class="info-address">Address: ' + self.address + ' / ' + self.address_fr + '</p>';
+            content += '<p class="info-ID">ID: ' + self.parkID;
+            content += '<p class="info-notes">NOTE: ' + self.notes;
+            self.infoWindow.setContent(content);
+        }
+
+        //Open infoWindow
+        self.infoWindow.open(map, self.mapMarker);
+    };
+
+    //Set object to active and make sure only one object active at a time
+    self.activate = function() {
+        if (SkateboardPark.prototype.active) {
+            if (SkateboardPark.prototype.active !== self) {
+                SkateboardPark.prototype.active.deactivate();
+            }
+        }
+        //populate InfoWindow
+        self.populateInfoWindow();
+        //Set animation of marker when activate
+        self.mapMarker.setAnimation(google.maps.Animation.BOUNCE);
+        //Set object to active
+        SkateboardPark.prototype.active = self;
+    };
+
+    //Close the infoWindow when its not active
+    self.deactivate = function() {
+        //Close infoWindow
+        self.infoWindow.close();
+        //Stop Bounce animation when deavtivate
+        self.mapMarker.setAnimation(null);
+        //Set object to none active
+        SkateboardPark.prototype.active = null;
+    };
+
+    //When click on the list, it will center the map to certain location
+    self.focus = function() {
+        map.panTo({
+            lat: self.latitude,
+            lng: self.longitude
+        });
+        self.activate();
+    };
+
+    //When click on the mapMarker it will change the status of infoWindow(populate/close)
+    self.mapMarker.addListener('click', function() {
+        if (SkateboardPark.prototype.active === self) {
+            self.deactivate();
+        } else {
+            self.activate();
+        }
+    });
+
+    //When click close button of infoWindow make sure the infoWindow is not active
+    self.infoWindow.addListener('closeclick', function() {
+        self.deactivate();
+    });
+}
+
+//View Model of the map
+function MapViewModel() {
+    var self = this;
+    self.parks = ko.observableArray([]);
+    self.filter = ko.observable('');
+    self.isVisible = ko.observable(false);
+
+    //Filter the results and change the visibility of mapMarker
+    self.filterResults = ko.computed(function() {
+        var results = [];
+
+        //Find matches using regular expression and ignore case
+        var re = new RegExp(self.filter(), 'i');
+
+        //Find matches by iterate all objects,
+        //Save object to results array if it's a match,
+        //If not match, hide mapMaker and deactive the infoWindow
+        self.parks().forEach(function(park) {
+            if (park.name.search(re) !== -1) {
+                results.push(park);
+                park.mapMarker.setVisible(true);
+            } else {
+                park.mapMarker.setVisible(false);
+                if (SkateboardPark.prototype.active === park) {
+                    park.deactivate();
+                }
+            }
+        });
+
+        return results;
+    });
+
+    //Change visibility when click toggle button
+    self.toggleVisibility = function() {
+        self.isVisible(!self.isVisible());
+    };
+
+    //When list is clicked
+    self.listClicked = function(park) {
+        park.focus();
+
+    };
+
+    //Get the data from API asynchronously
+    $.ajax({
+        url: 'http://query.yahooapis.com/v1/public/yql',
+        dataType: 'jsonp',
+        data: {
+            q: "select * from json where url=\"https://weekendchow.github.io/skateboard-parks-API/skateboard-parks.json\"",
+            format: "json"
+        },
+        success: function(data) {
+            data = data.query.results.json.features;
+
+            var parks = [];
+            var park;
+            var bounds = new google.maps.LatLngBounds();
+            data.forEach(function(dataObj) {
+                park = new SkateboardPark(dataObj);
+                parks.push(park);
+                bounds.extend(park.mapMarker.position);
+
+            });
+            // console.log(parks);
+            self.parks(parks);
+
+            map.fitBounds(bounds);
+
+        },
+        error: function() {
+            self.dealStatus('Oops, something went wrong, please reload the page and try again.');
+            self.loadImg('');
+        }
+    });
+
+}
